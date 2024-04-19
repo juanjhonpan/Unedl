869af0ae4e52417c529ebfe4c0d79ef3ab66beaa
juan <badopokor@hotmail.com>
2024-04-19 04:36:02 +0000

    Inicializar repositorio de ownCloud

---
+++ config/config.php
@@ -0,0 +1,38 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocukxywq9lm0',
+  'passwordsalt' => 'bQk4BI4q9KXRQNfn90FP3S/bl/Zm6Q',
+  'secret' => '8WUn2CQy/VEHTkoRBKQqCejzFgGSOlPgwqtrjiSVr1RlN0Xf',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.135.91',
+  ),
+  'datadirectory' => '/var/www/html/owncloud/data',
+  'overwrite.cli.url' => 'http://192.168.135.91/owncloud',
+  'dbtype' => 'mysql',
+  'version' => '10.12.1.3',
+  'dbname' => 'cpa_db',
+  'dbuser' => 'cpa_user',
+  'dbpassword' => 'Un3d3l@12',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'allow_user_to_change_mail_address' => '',
+  'logtimezone' => 'UTC',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/owncloud/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/owncloud/apps-external',
+      'url' => '/apps-external',
+      'writable' => true,
+    ),
+  ),
+  'installed' => true,
+);
---
+++ version.php
@@ -0,0 +1,8 @@
+<?php 
+$OC_Version = array(10,12,1,3);
+$OC_VersionString = '10.12.1';
+$OC_Channel = 'stable';
+$OC_Edition = 'community';
+$OC_VersionCanBeUpgradedFrom = [[8,2,11],[9,0,9],[9,1]];
+$OC_Build = '2023-04-15T12:17:13+00:00 97e3d82b92decc2d9307fad152cffa9bb5f31c44';
+$vendor = 'owncloud';
